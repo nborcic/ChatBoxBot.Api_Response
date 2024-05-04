1d7418e6943e809f80b48bd3ffa54f400217dc21
John Doe <nickborcic69@gmail.com>
2024-05-04 22:21:50 +0200

    displaying message without the storing all the message

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 //Path: script.js
 //leftDisp- 
 //rightDisp
-const inputElement = document.querySelector('#input');
-const displayElement = document.querySelector('#display');
-
-function sendMessage() {
-    const inputElement = document.querySelector('#input');
-    let message = inputElement.value;
-    const leftDiv = document.getElementById('leftDisp');
-    const rightDiv = document.getElementById('rightDisp');
-    const createP = document.createElement('p.1');
-    createP.style.fontSize = '40px';
-    rightDiv.appendChild(createP);
-    input.value = '';
-}
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
@@ -21,14 +8,25 @@ document.addEventListener('keydown', function (event) {
     }
 
 })
+function sendMessage() {
 
-//define the bubble for the message
-//define the picture to append the p element to
-const thereMessage = document.createElement('div.theremessage');
-thereMessage.style.display = 'flex';
+    const inputElement = document.querySelector('#input');
+    let message = inputElement.value;
+    const bubble = document.querySelector('.message, .sent');
+    const rightDiv = document.getElementById('rightDisp');
+    bubble.textContent = message;
+    rightDiv.appendChild(bubble);
+    input.value = '';
+    input.focus();
 
-//created massage and bubble sent by the user
-//and received by the user, respectively
-//created the p element to append the message to
-//bubble with respective styling for mine and theirs...going sleep now, its 1:59am
+}
+
+
+
+
+
+
+
+
+
 
